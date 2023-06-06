5b7be3280ef691eb001638e163293bb7e277323a
SofianeLasri <alasri250@gmail.com>
2023-06-06 22:18:13 +0200

    Modification sur les modèles, ajout du dossier uploads

--- WSR/admin.py
+++ WSR/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from WSR.models import *
+
 # Register your models here.
+admin.site.register(Article)
+admin.site.register(Course)
+admin.site.register(Resultat)
+admin.site.register(Vehicule)
+admin.site.register(Pilote)
+admin.site.register(Participation)
+admin.site.register(Saison)
--- WSR/models.py
+++ WSR/models.py
@@ -1,8 +1,10 @@
+import datetime
+
 from django.db import models
 
 class Saison(models.Model):
     nom = models.CharField(max_length=100)
-    annee = models.PositiveIntegerField()
+    annee = models.PositiveIntegerField(verbose_name="Année")
 
     def __str__(self):
         return self.nom
@@ -10,7 +12,7 @@ class Saison(models.Model):
 
 class Pilote(models.Model):
     nom = models.CharField(max_length=100)
-    vehicule = models.ForeignKey('Vehicule', on_delete=models.CASCADE)
+    vehicule = models.ForeignKey('Vehicule', on_delete=models.CASCADE, verbose_name="Véhicule")
 
     def __str__(self):
         return self.nom
@@ -18,18 +20,25 @@ class Pilote(models.Model):
 
 class Vehicule(models.Model):
     marque = models.CharField(max_length=100)
-    modele = models.CharField(max_length=100)
-    annee_fabrication = models.PositiveIntegerField()
+    modele = models.CharField(max_length=100, verbose_name="Modèle")
+    annee_fabrication = models.PositiveIntegerField(verbose_name="Année de fabrication")
 
     def __str__(self):
         return f"{self.marque} {self.modele}"
 
 
 class Course(models.Model):
+    class TypeCourse(models.TextChoices):
+        INVITATION = "invitation"
+        CHAMPIONNAT = "championnat"
+        COURSE_SIMPLE = "course_simple"
+
     nom = models.CharField(max_length=100)
     circuit = models.CharField(max_length=100)
     saison = models.ForeignKey(Saison, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='course_images')
+    image = models.ImageField(upload_to='course_images', verbose_name="Image d'illustration")
+    type = models.CharField(max_length=100, choices=TypeCourse.choices, default=TypeCourse.COURSE_SIMPLE)
+    date = models.DateField(default=datetime.date.today)
 
     def __str__(self):
         return self.nom
@@ -47,8 +56,9 @@ class Article(models.Model):
 class Resultat(models.Model):
     pilote = models.ForeignKey(Pilote, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    position_arrivee = models.PositiveIntegerField()
-    position_depart = models.PositiveIntegerField()
+    position_arrivee = models.PositiveIntegerField(verbose_name="Position à l'arrivée")
+    position_depart = models.PositiveIntegerField(verbose_name="Position de départ")
+    score = models.IntegerField()
 
     def __str__(self):
         return f"{self.pilote} - {self.course}"
